e567ad11f1d8801bd980cdf555d62dc1500a9794
Gilles Margerin <gillesmargerin@gmail.com>
2021-07-31 22:04:22 +0200

    passing tests for both exercises

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,12 +1,19 @@
+import assert from "assert/strict"
+
 function fizzBuzz(num) {
-  if (num % 5 === 0 && num % 3 === 0) {
-    console.log("Fizzbuzz");
+  if (num % 15 === 0) {
+    return "FizzBuzz";
   } else if (num % 5 === 0) {
-    console.log("Fizz");
+    return "Fizz";
   } else if (num % 3 === 0) {
-    console.log("Buzz");
+     return "Buzz";
   } else {
-    console.log(num);
+    return num;
   }
 }
+
+assert.deepStrictEqual(fizzBuzz(45), "FizzBuzz");
+assert.deepStrictEqual(fizzBuzz(10), "Fizz");
+assert.deepStrictEqual(fizzBuzz(33), "Buzz");
+assert.deepStrictEqual(fizzBuzz(11), 11)
 
--- incrCustomType.js
+++ incrCustomType.js
@@ -1,3 +1,27 @@
+import assert from "assert/strict";
+
 function incrCustomType(arr) {
-  console.log(parseInt(arr.join(''), 10) + 1);
+  let result = [];
+  let lastDigit = false;
+
+  for (let i = arr.length - 1, j = 0; i >= 0; i--, j++) {
+    if (lastDigit) {
+      result[j] = arr[i];
+    } else {
+      if (arr[i] < 9) {
+        result[j] = -(~arr[i]);
+        lastDigit = true;
+      } else {
+        result[j] = 0;
+        result[j + 1] = arr[i - 1] || 1;
+      }
+    }
+  }
+
+  return result.reverse();
 }
+
+
+assert.deepStrictEqual(incrCustomType([1, 2, 3]), [1, 2, 4]);
+assert.deepStrictEqual(incrCustomType([9, 9]), [1, 0, 0]);
+assert.deepStrictEqual(incrCustomType([9]), [1, 0]);
